ba05f64aec37e8a80c3a0853b4e3eeef9f72955e
Mahyar Moghimi Kian <mahyar.kian@gmail.com>
2018-01-14 00:41:16 +0100

    extention

--- src/pages/settings-popover/settings-popover.ts
+++ src/pages/settings-popover/settings-popover.ts
@@ -14,25 +14,53 @@ export class SettingsPopover {
     this.viewCtrl.dismiss();
   }
 
+  exporteren() {
+    let exporteren = this.alertCtrl.create({
+      title: 'Exporteren',
+      message: 'Naar welke email wilt u dat de data ge-exporteerd wordt?',
+      inputs: [
+        {
+          name: 'email',
+          placeholder: 'Email'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Annuleren',
+          role: 'cancel',
+          handler: data => {
+            console.log('Geanuleerd');
+          }
+        },
+        {
+          text: 'Exporteer data',
+          handler: data => {
+            console.log(data.email);
+          }
+        }
+      ]
+    });
+    exporteren.present();
+  }
   bevestigingTonen() {
-  let confirm = this.alertCtrl.create({
-    title: 'Lijst wissen?',
-    message: 'Opgelet! U staat op het punt deze lijst te wissen. Bent u zeker?',
-    buttons: [
-      {
-        text: 'Ja, doorgaan.',
-        handler: () => {
-          console.log('agreed clicked');
+    let confirm = this.alertCtrl.create({
+      title: 'Lijst wissen?',
+      message: 'Opgelet! U staat op het punt deze lijst te wissen. Bent u zeker?',
+      buttons: [
+        {
+          text: 'Ja, doorgaan',
+          handler: () => {
+            console.log('agreed clicked');
+          }
+        },
+        {
+          text: 'Neen, teruggaan',
+          handler: () => {
+            console.log('nope clicked');
+          }
         }
-      },
-      {
-        text: 'Neen, teruggaan.',
-        handler: () => {
-          console.log('nope clicked');
-        }
-      }
-    ]
-  });
-  confirm.present();
-    }
+      ]
+    });
+    confirm.present();
+  }
 }
